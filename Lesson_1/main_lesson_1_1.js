8f8855f42259fc0da914f032c8883bb28dd1b4aa
IrynaHubka <iren.grigo@gmail.com>
2023-05-02 12:22:17 +0300

    add main-JS_lesson_1_1

---
+++ Lesson_1/main_lesson_1_1.js
@@ -0,0 +1,29 @@
+let s1 = 'hello'
+console.log(s1)
+let s2 = 'owu'
+console.log(s2)
+let s3 = 'com'
+console.log(s3)
+let s4 = 'ua'
+console.log(s4)
+let s5 = 'true'
+console.log(s5)
+let s6 = 'false'
+console.log(s6)
+let n1 = 1
+console.log(n1)
+let n2 = 10
+console.log(n2)
+let n3 = -999
+console.log(n3)
+let n4 = 123
+console.log(n4)
+let n5 = 3.14
+console.log(n5)
+let n6 = 2.7
+console.log(n6)
+let n7 = 16
+console.log(n7)
+
+
+
