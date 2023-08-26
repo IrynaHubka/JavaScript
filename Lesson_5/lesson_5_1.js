c89c2612f78e1314fbd6f4dadab3da41a9c9a63c
IrynaHubka <iren.grigo@gmail.com>
2023-08-26 13:13:34 +0300

    add lesson_2_2_Js

--- Lesson_5/lesson_5_1.js
+++ Lesson_5/lesson_5_1.js
@@ -6,10 +6,62 @@
 // console.log(result);
 // let nom1 = ['name','sssd','sdsdad','asdassad','fdgdffg'];
 // console.log(nom1);
-let dooks = {
-    title:'Ann',
-    page: 127,
-    genry: "detective",
-    avtors: ['title',"age"],
-}
-console.log(dooks)
+// let dooks = {
+//     title:'Ann',
+//     page: 127,
+//     genry: "detective",
+//     avtors: [
+//         {name: "Anna",age: 25},
+//     ]
+// }
+// console.log(dooks)
+// let users = [
+//      { name:'Anna',username: 'Pupkina', pasword: 2020},
+//      { name:'Vova', username: 'Pupkin', pasword: 2025},
+// ]
+// console.log(users[0].pasword)
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+// let x = 25;
+// if (x !== 25) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+// let time = 40;
+// if (time >0 && time <=15) {
+//     console.log('перша частина години');
+// } else if (time >15 && time <= 30) {
+//     console.log('друга частина години');
+// } else if (time >30 && time <= 45) {
+//     console.log('третя частина години');
+// } else if (time >45 && time <=59) {
+//     console.log('четверта частина години');
+// }
+//
+//У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+// let day = 25;
+// if (day >0 && day <=10) {
+//     console.log('Перша декада місяця');
+// } else if (day >10 && day <=20) {
+//     console.log('Друга декада місяця');
+// }else if (day >20 && day<=31) {
+//     console.log('Третя декада місяця')
+// }
+// let dayWeek = 3;
+// switch (dayWeek) {
+//     case 1:
+//         console.log('Monday');
+//         break
+//     case 2:
+//         console.log('Thursday');
+//         break
+//     case 3:
+//         console.log('Theerday');
+//         break
+//     default:
+//         console.log('????')
+// }
+/
