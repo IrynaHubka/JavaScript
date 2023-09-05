5764b847964d3d54305b11f7fc4c47602cf58b77
IrynaHubka <iren.grigo@gmail.com>
2023-09-05 16:46:34 +0300

    add lesson_3_2_Js

--- Lesson_3/js_lesson3_2.js
+++ Lesson_3/js_lesson3_2.js
@@ -51,29 +51,30 @@ let users = [
     {name: 'petya', age: 30, status: true},
     {name: 'kolya', age: 29, status: true},
     {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
+    {name: 'max', age: 34, status: true},
+    {name: 'anya', age: 30, status: false},
     {name: 'oleg', age: 28, status: false},
     {name: 'andrey', age: 29, status: true},
     {name: 'masha', age: 30, status: true},
     {name: 'olya', age: 31, status: false},
     {name: 'max', age: 31, status: true}
 ];
-document.write('<h3>True:</h3>');
-for (let i = 0; i < users.length; i++) {
-    if (users[i].status === true) {
-        document.write(users[i].name + '<br>');
+//  - true
+
+for (const user of users) {
+    if (user.status) {
+        document.write(`<div>Name: ${user.name} <br/> Age: ${user.age} Status: ${user.status}</div>`);
     }
 }
-document.write('<h3>False:</h3>');
-for (let i = 0; i < users.length; i++) {
-    if (users[i].status === false) {
-        document.write(users[i].name + '<br>');
+// - false
+for (const user of users) {
+    if (!user.status) {
+        document.write(`<div>Name: ${user.name} <br/> Age: ${user.age} Status: ${user.status}</div>`);
     }
 }
-document.write('<h3>Older than 30 years:</h3>');
-for (let i = 0; i < users.length; i++) {
-    if (users[i].age > 30) {
-        document.write(users[i].name + '<br>');
+// -  старші за 30 років
+for (const user of users) {
+    if (user.age > 30) {
+        document.write(`<div>Name: ${user.name} <br/> Age: ${user.age} Status: ${user.status}</div>`);
     }
 }
