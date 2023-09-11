87ed2e3a47218fa34fd39ca667167d07ae14c1e1
IrynaHubka <iren.grigo@gmail.com>
2023-09-11 17:56:27 +0300

    add lesson_4_2_Js

--- Lesson_4/lesson_4_2.js
+++ Lesson_4/lesson_4_2.js
@@ -1,80 +1,23 @@
-/*- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок. */
-function renderUsers(users) {
-    const container = document.createElement('div');
-
-    users.forEach(user => {
-        const userBlock = document.createElement('div');
-        const userId = document.createElement('p');
-        const userName = document.createElement('p');
-        const userAge = document.createElement('p');
-
-        userId.textContent = `Id: ${user.id}`;
-        userName.textContent = `Name: ${user.name}`;
-        userAge.textContent = `Age: ${user.age}`;
-
-        userBlock.appendChild(userId);
-        userBlock.appendChild(userName);
-        userBlock.appendChild(userAge);
-
-        container.appendChild(userBlock);
-    });
-
-    document.body.appendChild(container);
-}
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 const users = [
-    { id: 1, name: 'Anna', age: 25 },
-    { id: 2, name: 'Vova', age: 30 },
-    { id: 3, name: 'Igor', age: 35 },
+    {id: 1, name: 'vasya', age: 31},
+    {id: 2, name: 'petya', age: 30},
+    {id: 3, name: 'kolya', age: 29},
+    {id: 4, name: 'olya', age: 28},
+    {id: 5, name: 'max', age: 30},
+    {id: 6, name: 'anya', age: 31},
+    {id: 7, name: 'oleg', age: 28},
+    {id: 8, name: 'andrey', age: 29},
+    {id: 9, name: 'masha', age: 30},
+    {id: 10, name: 'olya', age: 31},
+    {id: 11, name: 'max', age: 31}
 ];
-renderUsers(users);
 
-/* - створити функцію яка повертає найменьше число з масиву */
-function findSmallestNumber(numbers) {
-    return Math.min(...numbers);
-}
-let numbers = [4, 7, 1, 8, 3];
-let smallestNumber = findSmallestNumber(numbers);
-console.log(smallestNumber);
-
-/* - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13 */
-function sum(arr) {
-    let result = 0;
-    for (let i = 0; i < arr.length; i++) {
-        result += arr[i];
+function usersBlok(arr) {
+    for (const user of arr) {
+        document.write(`<div>id: ${user.id} - name: ${user.name} - age: ${user.age}</div>`);
     }
-    return result;
 }
-console.log(sum([1, 2, 10]));
 
-/* - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
-Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44] */
-function swap(arr, index1, index2) {
-    let temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-    return arr;
-}
-let arr = [11, 22, 33, 44];
-swap(arr, 0, 1);
-console.log(arr); // [22, 11, 33, 44]
-
-/* - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
-Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250 */
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
-    const currencyToBuy = currencyValues.find(currency => currency.currency === exchangeCurrency);
-    const sumInExchangeCurrency = sumUAH / currencyToBuy.value;
-    return sumInExchangeCurrency;
-}
-const currencyValues = [
-    { currency: 'USD', value: 40 },
-    { currency: 'EUR', value: 42 }
-];
-const sumUAH = 10000;
-const exchangeCurrency = 'USD';
-const sumInExchangeCurrency = exchange(sumUAH, currencyValues, exchangeCurrency);
-console.log(sumInExchangeCurrency)
-
-
-
-
+usersBlok(users);
 
